f119d1cad77484bdd2964d4c951ac394c397db52
Mostafaamin1000 <mostafaamin09098@mail.com>
2025-05-15 23:51:58 -0700

    add validation to auth

--- src/Modules/auth/auth.router.js
+++ src/Modules/auth/auth.router.js
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import { signin, signup } from "./auth.controller.js";
 import { checkEmail } from "../../middlewares/checkemail.js";
+import { Validate } from "../../middlewares/validate.js";
+import { changePasswordVal, signinVal, signupVal } from "./auth.validation.js";
 
 
 const authRouter=Router()
 
 
-authRouter.post('/signup',checkEmail,signup)
-authRouter.post('/signin',signin)
-// authRouter.patch('/changepassword',Validate(changePasswordVal),changeUserPassword)
-// // 💡 Forgot Password
-// authRouter.post('/forget-password',forgetPassword)
-// // 🔄 Reset Password
-// authRouter.post('/reset-password/:token',resetPassword)
+authRouter.post('/signup',Validate(signupVal),checkEmail,signup)
+authRouter.post('/signin',Validate(signinVal),signin)
+authRouter.patch('/changepassword',Validate(changePasswordVal),changeUserPassword)
+authRouter.post('/forget-password',forgetPassword)
+authRouter.post('/reset-password/:token',resetPassword)
     
 export default authRouter   
